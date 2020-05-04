6228d4f7b8d8463a6a36989ed97e6709781a02bd
Karthik <karthikswaroopz@gmail.com>
2020-05-04 18:21:53 +0530

    Updates

---
+++ precache-manifest.d65f78b85ada26fbfdbd5fdc58ad17ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5081c1d6372880eaae1390f229a58e55",
+    "url": "/Tamilnadu-COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "675ea943c70c97e4b838",
+    "url": "/Tamilnadu-COVID19-Tracker/static/css/main.26406362.chunk.css"
+  },
+  {
+    "revision": "8b6454db4100ba0db9be",
+    "url": "/Tamilnadu-COVID19-Tracker/static/js/2.7b12fd1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tamilnadu-COVID19-Tracker/static/js/2.7b12fd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675ea943c70c97e4b838",
+    "url": "/Tamilnadu-COVID19-Tracker/static/js/main.56a5deb9.chunk.js"
+  },
+  {
+    "revision": "b553c206bd3efed30a0e",
+    "url": "/Tamilnadu-COVID19-Tracker/static/js/runtime-main.a3a34e29.js"
+  }
+]);
